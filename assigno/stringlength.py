3a8a572bdee0a96343bf1dc820ed222b45e266d6
misoi <cytnhia.chepkemoi@gmail.com>
2016-08-17 10:22:45 +0300

    a function that takes in a string or a list and gives the length

--- assigno/stringlength.py
+++ assigno/stringlength.py
@@ -1,9 +1,10 @@
 
-def stringlemgth(str):
-	group = []	
-	for i in str:
-		group.append(len(i))
-		return group
-
-str = ["Hellen","Joyce","Beatrice"]
-print stringlemgth(str)
+def string_length(y):  # define a function that takes in a string
+  n = [] #an empty list
+  if type(y) == str: #check if the input is a string 
+    n.append(len(y)) #add the length of the string to an empty list
+    return n #return the list 
+  elif type(y) == list: #if the input is a list
+    for item in y: #loop through every item of a list and gets its number
+      n.append(len(item)) #append the length of every item to the empty list
+    return n # returns the list
